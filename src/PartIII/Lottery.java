194ef8e2fd97e3d408a9c36e869a3eda3d21fef1
David Manouchehri <david@davidmanouchehri.com>
2015-06-05 21:29:39 -0400

    Bulk of the work done. Incorrect value for Cinderella/Rapunzel.

--- src/PartIII/Lottery.java
+++ src/PartIII/Lottery.java
@@ -8,5 +8,38 @@ package PartIII;
  *
  *         Use algorithm from Part I, output should be formatted the same.
  */
+
+import java.util.Scanner;
+
 public class Lottery {
+    public static void main(String[] args) {
+        /* All percentages in decimal */
+        final double taxRate = 0.12;
+        final double snowShare = 0.20;
+        final double dwarfShare = 0.20;
+        final double cinderellaShare = 0.10;
+        final double rapunzelShare = 0.10;
+
+        Scanner userInput = new Scanner(System.in);
+        System.out.print("Please enter amount won: ");
+        double winnings = userInput.nextDouble();
+
+        double tax = winnings * taxRate;
+        double postTax = winnings - tax;
+
+        double snow = postTax * snowShare;
+        double postSnow = postTax - snow;
+
+        double dwarf = postSnow * dwarfShare;
+        double postDwarf = postSnow - dwarf;
+
+        double cinderella = postDwarf * cinderellaShare;
+        double rapunzel = postDwarf * rapunzelShare;
+
+        System.out.printf("Winner's Tax: %.1f\n", tax);
+        System.out.printf("Snow White's Share: %.1f\n", snow);
+        System.out.printf("Each Dwarf Share: %.1f\n", dwarf);
+        System.out.printf("Cinderella's Share: %.1f\n", cinderella);
+        System.out.printf("Rapunzel's Share: %.1f", rapunzel);
+    }
 }
